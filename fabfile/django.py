893c75e6cd2a194cc33318075491b1523e0145b7
Tyler Fisher <tfisher@politico.com>
2018-01-09 16:24:51 -0500

    run it on the server

--- fabfile/django.py
+++ fabfile/django.py
@@ -1,16 +1,6 @@
-from fabric.api import local, task
-
-
-@task
-def migrate_db():
-    local('python manage.py migrate')
-
-
-@task
-def collectstatic():
-    local('python manage.py collectstatic')
+from fabric.api import run, task
 
 
 @task
 def management(cmd):
-    local('python manage.py {0}'.format(cmd))
+    run('python manage.py {0}'.format(cmd))
