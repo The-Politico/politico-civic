d962d46b7494b64ce2ce77090994a3443339f915
Tyler Fisher <tfisher@politico.com>
2018-01-10 11:13:24 -0500

    use basedir to find env file

--- civic/settings.py
+++ civic/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import environ
 import os
-env = environ.Env(DEBUG=(bool, True),)
-environ.Env.read_env('../.env')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env = environ.Env(DEBUG=(bool, True),)
+environ.Env.read_env('{0}/.env'.format(BASE_DIR))
+
 SECRET_KEY = '4)xde(1$&het&k-_ewwe=w#dy55e9e8jq%zm+s(^b^(bv+6a1%'
 ALLOWED_HOSTS = ['18.216.126.224', 'politicoapps.com', 'localhost']
 
