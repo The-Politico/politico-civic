def65a0378903722955a31b4eb43846eaf010810
Jon McClure <jmcclure@politico.com>
2018-01-13 08:32:30 -0500

    add social auth. remove project dependencies for now

--- civic/settings.py
+++ civic/settings.py
@@ -10,17 +10,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
-import environ
 import os
 
+import environ
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-env = environ.Env()
+env = environ.Env(DEBUG=(bool, False),)
 environ.Env.read_env('{0}/.env'.format(BASE_DIR))
 
-SECRET_KEY = '4)xde(1$&het&k-_ewwe=w#dy55e9e8jq%zm+s(^b^(bv+6a1%'
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 ALLOWED_HOSTS = ['18.216.126.224', 'politicoapps.com', 'localhost']
 
-DEBUG = False
+DEBUG = env('DEBUG')
 
 # Application definition
 
@@ -31,13 +32,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'entity',
-    'geography',
-    'government',
-    'election',
-    'vote',
-    'stump',
-    'poll',
+    'social_django',
+    'rest_framework',
+    # 'entity',
+    # 'geography',
+    # 'government',
+    # 'election',
+    # 'vote',
+    # 'stump',
+    # 'poll',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +67,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -85,43 +90,48 @@ else:
 
 
 # Password validation
-# https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa E501
     },
 ]
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/2.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
 WHITENOISE_STATIC_PREFIX = '/static/'
-print(BASE_DIR)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+# SOCIAL AUTH
+
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.slack.SlackOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+LOGIN_URL = '/login/slack/'
+SOCIAL_AUTH_SLACK_KEY = os.getenv('SOCIAL_AUTH_SLACK_KEY')
+SOCIAL_AUTH_SLACK_SECRET = os.getenv('SOCIAL_AUTH_SLACK_SECRET')
+SOCIAL_AUTH_SLACK_TEAM = 'politicobn'
+SOCIAL_AUTH_USERNAME_IS_FULL_EMAIL = True
+SOCIAL_AUTH_REDIRECT_IS_HTTPS = False if DEBUG else True
