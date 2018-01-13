93a2c98cae2aa9a80249614dc4ea1cd441da73e6
Jon McClure <jmcclure@politico.com>
2018-01-13 18:37:01 -0500

    try stringing bash commands together

--- deploy/postinstall.sh
+++ deploy/postinstall.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
+export PIPENV_VENV_IN_PROJECT=1
+export PIPENV_IGNORE_VIRTUALENVS=1
 sudo service awslogs restart
 cd /home/ubuntu/apps/politico-civic
 source /home/ubuntu/.bash_profile
 pyenv global general
-pipenv install
-pipenv run python manage.py migrate
-pipenv run python manage.py collectstatic
+pipenv install &&
+pipenv run python manage.py migrate &&
+pipenv run python manage.py collectstatic &&
 sudo service politico-civic.uwsgi restart
