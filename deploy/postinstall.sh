db1bc9aeb0d6e3492301d6111bf7e2180672bdff
Jon McClure <jmcclure@politico.com>
2018-01-13 17:36:29 -0500

    try sudoing pipenv install

--- deploy/postinstall.sh
+++ deploy/postinstall.sh
@@ -3,7 +3,7 @@ sudo service awslogs restart
 cd /home/ubuntu/apps/politico-civic
 source /home/ubuntu/.bash_profile
 pyenv global general
-pipenv install
+sudo pipenv install
 pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic
 sudo service politico-civic.uwsgi restart
