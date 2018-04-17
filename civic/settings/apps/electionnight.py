75e135fc1f40345f77552e99740da47d02e591ff
Tyler Fisher <tfisher@politico.com>
2018-04-17 17:51:47 -0400

    bump electionnight version

--- civic/settings/apps/electionnight.py
+++ civic/settings/apps/electionnight.py
@@ -4,7 +4,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 ELECTIONNIGHT_AWS_S3_BUCKET = os.getenv('ELECTIONNIGHT_AWS_S3_BUCKET')
 ELECTIONNIGHT_RESULTS_STATIC_DIR = 'static_results'
-ELECTIONNIGHT_AWS_S3_STATIC_ROOT = "https://s3.amazonaws.com/staging.interactives.politico.com"
+ELECTIONNIGHT_AWS_S3_STATIC_ROOT = os.getenv('ELECTIONNIGHT_AWS_S3_STATIC_ROOT')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, ELECTIONNIGHT_RESULTS_STATIC_DIR)
 ]
