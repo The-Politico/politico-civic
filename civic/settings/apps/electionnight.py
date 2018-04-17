12d92bc389a711272dce6078976f03e1f21442b9
Tyler Fisher <tfisher@politico.com>
2018-04-17 17:22:45 -0400

    get elections baking properly

--- civic/settings/apps/electionnight.py
+++ civic/settings/apps/electionnight.py
@@ -1,3 +1,10 @@
 import os
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 ELECTIONNIGHT_AWS_S3_BUCKET = os.getenv('ELECTIONNIGHT_AWS_S3_BUCKET')
+ELECTIONNIGHT_RESULTS_STATIC_DIR = 'static_results'
+ELECTIONNIGHT_AWS_S3_STATIC_ROOT = "https://s3.amazonaws.com/staging.interactives.politico.com"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, ELECTIONNIGHT_RESULTS_STATIC_DIR)
+]
