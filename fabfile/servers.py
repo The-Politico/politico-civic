6ee71b46f9282acaca7f5851402167913d012a7c
Tyler Fisher <tfisher@politico.com>
2018-10-17 22:15:51 -0400

    fix onespot commands for new management commands

--- fabfile/servers.py
+++ fabfile/servers.py
@@ -35,28 +35,34 @@ def setup():
 
 @task
 def install_extra_requirements():
-    sudo('apt-get install jq -y')
-    run('pip install awscli')
-    sudo('npm install topojson')
+    sudo("apt-get install jq -y")
+    run("pip install awscli")
+    sudo("npm install topojson")
 
 
 @task
-def checkout_latest(remote='origin'):
+def checkout_latest(remote="origin"):
     """
     Checkout the latest source.
     """
-    with cd('%(SERVER_PROJECT_PATH)s' % server_config.__dict__):
-        run('cd %s; git fetch %s' % (
-            server_config.SERVER_PROJECT_PATH, remote
-        ))
-        run('cd %s; git checkout %s; git pull %s %s' % (
-            server_config.SERVER_PROJECT_PATH, env.branch, remote, env.branch
-        ))
-        run('pyenv global 3.6.4')
-        run('pip install -r requirements.txt')
-        run('python manage.py migrate')
-        run('python manage.py collectstatic --noinput')
-        restart_service('uwsgi')
+    with cd("%(SERVER_PROJECT_PATH)s" % server_config.__dict__):
+        run(
+            "cd %s; git fetch %s" % (server_config.SERVER_PROJECT_PATH, remote)
+        )
+        run(
+            "cd %s; git checkout %s; git pull %s %s"
+            % (
+                server_config.SERVER_PROJECT_PATH,
+                env.branch,
+                remote,
+                env.branch,
+            )
+        )
+        run("pyenv global 3.6.4")
+        run("pip install -r requirements.txt")
+        run("python manage.py migrate")
+        run("python manage.py collectstatic --noinput")
+        restart_service("uwsgi")
 
 
 @task
@@ -64,9 +70,9 @@ def setup_logs():
     """
     Create log directories.
     """
-    sudo('mkdir %(SERVER_LOG_PATH)s' % server_config.__dict__)
-    sudo('touch %(SERVER_LOG_PATH)s/django.log' % server_config.__dict__)
-    sudo('chown ubuntu:ubuntu %(SERVER_LOG_PATH)s' % server_config.__dict__)
+    sudo("mkdir %(SERVER_LOG_PATH)s" % server_config.__dict__)
+    sudo("touch %(SERVER_LOG_PATH)s/django.log" % server_config.__dict__)
+    sudo("chown ubuntu:ubuntu %(SERVER_LOG_PATH)s" % server_config.__dict__)
 
 
 @task
@@ -74,7 +80,7 @@ def setup_cert():
     """
     Create SSL certificate on the server
     """
-    sudo('certbot --nginx -d {} certonly'.format(env.hosts[0]))
+    sudo("certbot --nginx -d {} certonly".format(env.hosts[0]))
 
 
 @task
@@ -82,9 +88,9 @@ def renew_cert():
     """
     Renew SSL certificate on the server
     """
-    sudo('service nginx stop')
-    sudo('certbot renew')
-    sudo('service nginx start')
+    sudo("service nginx stop")
+    sudo("certbot renew")
+    sudo("service nginx start")
 
 
 @task
@@ -92,7 +98,7 @@ def delete_project():
     """
     Remove the project directory. Invoked by shiva.
     """
-    run('rm -rf %(SERVER_PROJECT_PATH)s' % server_config.__dict__)
+    run("rm -rf %(SERVER_PROJECT_PATH)s" % server_config.__dict__)
 
 
 """
@@ -104,15 +110,17 @@ def _get_template_conf_path(service, extension):
     """
     Derive the path for a conf template file.
     """
-    return 'confs/%s.%s' % (service, extension)
+    return "confs/%s.%s" % (service, extension)
 
 
 def _get_rendered_conf_path(service, extension):
     """
     Derive the rendered path for a conf file.
     """
-    return 'confs/rendered/%s.%s.%s' % (
-        server_config.PROJECT_FILENAME, service, extension
+    return "confs/rendered/%s.%s.%s" % (
+        server_config.PROJECT_FILENAME,
+        service,
+        extension,
     )
 
 
@@ -120,8 +128,11 @@ def _get_installed_conf_path(service, remote_path, extension):
     """
     Derive the installed path for a conf file.
     """
-    return '%s/%s.%s.%s' % (
-        remote_path, server_config.PROJECT_FILENAME, service, extension
+    return "%s/%s.%s.%s" % (
+        remote_path,
+        server_config.PROJECT_FILENAME,
+        service,
+        extension,
     )
 
 
@@ -129,7 +140,7 @@ def _get_installed_service_name(service):
     """
     Derive the init service name for an installed service.
     """
-    return '%s.%s' % (server_config.PROJECT_FILENAME, service)
+    return "%s.%s" % (server_config.PROJECT_FILENAME, service)
 
 
 @task
@@ -141,19 +152,19 @@ def render_confs():
     # Copy the server_config so that when we load the secrets they don't
     # get exposed to other management commands
     context = copy.copy(server_config.__dict__)
-    context['SERVERS'] = env.hosts
-    if env.hosts[0].endswith('.com'):
-        context['SSL'] = True
+    context["SERVERS"] = env.hosts
+    if env.hosts[0].endswith(".com"):
+        context["SSL"] = True
     else:
-        context['SSL'] = False
+        context["SSL"] = False
 
     for service, remote_path, extension in server_config.SERVER_SERVICES:
         template_path = _get_template_conf_path(service, extension)
         rendered_path = _get_rendered_conf_path(service, extension)
 
-        with open(template_path, 'r') as read_template:
+        with open(template_path, "r") as read_template:
 
-            with open(rendered_path, 'w') as write_template:
+            with open(rendered_path, "w") as write_template:
                 payload = Template(read_template.read())
                 write_template.write(payload.render(**context))
 
@@ -173,31 +184,35 @@ def deploy_confs():
                 service, remote_path, extension
             )
 
-            a = local('md5 -q %s' % rendered_path, capture=True)
-            b = run('md5sum %s' % installed_path).split()[0]
+            a = local("md5 -q %s" % rendered_path, capture=True)
+            b = run("md5sum %s" % installed_path).split()[0]
 
             if a != b:
-                logging.info('Updating %s' % installed_path)
+                logging.info("Updating %s" % installed_path)
                 put(rendered_path, installed_path, use_sudo=True)
 
-                if service == 'nginx':
-                    sudo('rm /etc/nginx/sites-enabled/%s.nginx.conf' %
-                         server_config.PROJECT_FILENAME)
-                    sudo('ln -s /etc/nginx/sites-available/%s.nginx.conf'
-                         ' /etc/nginx/sites-enabled' %
-                         server_config.PROJECT_FILENAME)
-                    sudo('service nginx restart')
-                elif service == 'uwsgi':
+                if service == "nginx":
+                    sudo(
+                        "rm /etc/nginx/sites-enabled/%s.nginx.conf"
+                        % server_config.PROJECT_FILENAME
+                    )
+                    sudo(
+                        "ln -s /etc/nginx/sites-available/%s.nginx.conf"
+                        " /etc/nginx/sites-enabled"
+                        % server_config.PROJECT_FILENAME
+                    )
+                    sudo("service nginx restart")
+                elif service == "uwsgi":
                     service_name = _get_installed_service_name(service)
-                    sudo('initctl reload-configuration')
-                    sudo('service %s restart' % service_name)
-                elif service == 'app':
-                    sudo('mkdir /run/uwsgi/')
-                    sudo('touch %s' % server_config.UWSGI_SOCKET_PATH)
-                    sudo('chmod 777 %s' % server_config.UWSGI_SOCKET_PATH)
-                    sudo('chown ubuntu:www-data /run/uwsgi/')
+                    sudo("initctl reload-configuration")
+                    sudo("service %s restart" % service_name)
+                elif service == "app":
+                    sudo("mkdir /run/uwsgi/")
+                    sudo("touch %s" % server_config.UWSGI_SOCKET_PATH)
+                    sudo("chmod 777 %s" % server_config.UWSGI_SOCKET_PATH)
+                    sudo("chown ubuntu:www-data /run/uwsgi/")
             else:
-                logging.info('%s has not changed' % rendered_path)
+                logging.info("%s has not changed" % rendered_path)
 
 
 @task
@@ -212,16 +227,16 @@ def nuke_confs():
                 service, remote_path, extension
             )
 
-            sudo('rm -f %s' % installed_path)
+            sudo("rm -f %s" % installed_path)
 
-            if service == 'nginx':
-                sudo('service nginx reload')
-            elif service == 'uwsgi':
+            if service == "nginx":
+                sudo("service nginx reload")
+            elif service == "uwsgi":
                 service_name = _get_installed_service_name(service)
-                sudo('service %s stop' % service_name)
-                sudo('initctl reload-configuration')
-            elif service == 'app':
-                sudo('rm %s' % server_config.UWSGI_SOCKET_PATH)
+                sudo("service %s stop" % service_name)
+                sudo("initctl reload-configuration")
+            elif service == "app":
+                sudo("rm %s" % server_config.UWSGI_SOCKET_PATH)
 
 
 @task
@@ -230,23 +245,41 @@ def start_service(service):
     Start a service on the server.
     """
     service_name = _get_installed_service_name(service)
-    sudo('service %s start' % service_name)
+    sudo("service %s start" % service_name)
 
 
 @task
-def start_results(date, test='', replay=''):
-    service_name = _get_installed_service_name('results')
-    sudo('service {0} start DATE={1} TEST={2} REPLAY={3}'.format(
-        service_name, date, test, replay
-    ))
+def start_results(date, test="", replay=""):
+    state = _get_installed_service_name("state_results")
+    sudo(
+        "service {0} start DATE={1} TEST={2} REPLAY={3}".format(
+            state, date, test, replay
+        )
+    )
+
+    county = _get_installed_service_name("county_results")
+    sudo(
+        "service {0} start DATE={1} TEST={2} REPLAY={3}".format(
+            county, date, test, replay
+        )
+    )
 
 
 @task
-def start_reup(date, no_bot=''):
-    service_name = _get_installed_service_name('reup')
-    sudo('service {0} start DATE={1} BOT={2}'.format(
-        service_name, date, no_bot
-    ))
+def stop_results():
+    state = _get_installed_service_name("state_results")
+    sudo("service {0} stop".format(state))
+
+    county = _get_installed_service_name("county_results")
+    sudo("service {0} stop".format(county))
+
+
+@task
+def start_reup(date, no_bot=""):
+    service_name = _get_installed_service_name("reup")
+    sudo(
+        "service {0} start DATE={1} BOT={2}".format(service_name, date, no_bot)
+    )
 
 
 @task
@@ -255,7 +288,7 @@ def stop_service(service):
     Stop a service on the server
     """
     service_name = _get_installed_service_name(service)
-    sudo('service %s stop' % service_name)
+    sudo("service %s stop" % service_name)
 
 
 @task
@@ -264,7 +297,7 @@ def restart_service(service):
     Start a service on the server.
     """
     service_name = _get_installed_service_name(service)
-    sudo('service %s restart' % service_name)
+    sudo("service %s restart" % service_name)
 
 
 @task
@@ -273,5 +306,7 @@ def fabcast(command):
     Actually run specified commands on the server specified
     by staging() or production().
     """
-    run('cd %s && bash run_on_server.sh pipenv run fab %s %s'
-        % (server_config.SERVER_PROJECT_PATH, env.branch, command))
+    run(
+        "cd %s && bash run_on_server.sh pipenv run fab %s %s"
+        % (server_config.SERVER_PROJECT_PATH, env.branch, command)
+    )
--- onespot/cli.py
+++ onespot/cli.py
@@ -22,69 +22,69 @@ def election():
     """Manages election nights."""
 
 
-@server.command('init')
+@server.command("init")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def server_init(target):
     """
     Creates a new server through terraform
     """
-    os.chdir('terraform/{}'.format(target))
-    run(['terraform', 'init'])
+    os.chdir("terraform/{}".format(target))
+    run(["terraform", "init"])
 
 
-@server.command('launch')
+@server.command("launch")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def server_launch(target):
     """
     Creates a new server through terraform
     """
-    os.chdir('terraform/{}'.format(target))
-    run(['terraform', 'apply'])
+    os.chdir("terraform/{}".format(target))
+    run(["terraform", "apply"])
 
 
-@server.command('setup')
+@server.command("setup")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def server_setup(target):
     """
     Setup an existing server with Fabric
     """
-    run(['fab', target, 'servers.setup'])
+    run(["fab", target, "servers.setup"])
 
 
-@server.command('update')
+@server.command("update")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def server_update(target):
     """
     Update repo on server
     """
-    run(['fab', target, 'master', 'servers.checkout_latest'])
+    run(["fab", target, "master", "servers.checkout_latest"])
 
 
-@server.command('destroy')
+@server.command("destroy")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def server_destroy(target):
     """
     Destroys server architecture using terraform
     """
-    os.chdir('terraform/{}'.format(target))
-    run(['terraform', 'destroy'])
+    os.chdir("terraform/{}".format(target))
+    run(["terraform", "destroy"])
 
 
-@election.command('init')
-@click.argument('date')
-@click.option('--test', is_flag=True, help='Pass test flag to elex')
+@election.command("init")
+@click.argument("date")
+@click.option("--test", is_flag=True, help="Pass test flag to elex")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def election_init(date, target, test):
     """
@@ -92,112 +92,126 @@ def election_init(date, target, test):
     and bootstraps content in the database.
     """
     if test:
-        test_flag = '--test'
+        test_flag = "--test"
     else:
-        test_flag = ''
+        test_flag = ""
 
-    bootstrap_elex = 'bootstrap_elex {0} {1}'.format(date, test_flag)
-    bootstrap_results_config = 'bootstrap_results_config {0}'.format(date)
-    bootstrap_content = 'bootstrap_content {0}'.format(date)
+    bootstrap_elex = "bootstrap_elex {0} {1}".format(date, test_flag)
+    bootstrap_results_config = "bootstrap_results_config {0}".format(date)
+    bootstrap_content = "bootstrap_content {0}".format(date)
 
-    run(['fab', target, 'django.management:{0}'.format(bootstrap_elex)])
-    run([
-        'fab', target, 'django.management:{0}'.format(bootstrap_results_config)
-    ])
-    run(['fab', target, 'django.management:{0}'.format(bootstrap_content)])
+    run(["fab", target, "django.management:{0}".format(bootstrap_elex)])
+    run(
+        [
+            "fab",
+            target,
+            "django.management:{0}".format(bootstrap_results_config),
+        ]
+    )
+    run(["fab", target, "django.management:{0}".format(bootstrap_content)])
 
 
-@election.command('start')
-@click.argument('date')
-@click.option('--test', is_flag=True, help='Pass test flag to elex')
-@click.option('--nobot', is_flag=True, help='Pass no-bot flag to reup')
+@election.command("start")
+@click.argument("date")
+@click.option("--test", is_flag=True, help="Pass test flag to elex")
+@click.option("--nobot", is_flag=True, help="Pass no-bot flag to reup")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def election_start(date, target, test, nobot):
     """
     Starts results and reup processes on the server.
     """
     if test:
-        test_flag = '--test'
+        test_flag = "--test"
     else:
-        test_flag = ''
+        test_flag = ""
 
     if nobot:
-        no_bot_flag = '--nobot'
+        no_bot_flag = "--nobot"
     else:
-        no_bot_flag = ''
+        no_bot_flag = ""
 
-    run(['fab', target, 'servers.start_results:{0},{1}'.format(
-        date, test_flag
-    )])
-    run(['fab', target, 'servers.start_reup:{0},{1}'.format(date, no_bot_flag)])
+    run(
+        [
+            "fab",
+            target,
+            "servers.start_results:{0},{1}".format(date, test_flag),
+        ]
+    )
+    run(
+        ["fab", target, "servers.start_reup:{0},{1}".format(date, no_bot_flag)]
+    )
 
 
-@election.command('stop')
+@election.command("stop")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def election_stop(target):
     """
     Stops results and reup processes on the server.
     """
-    run(['fab', target, 'servers.stop_service:results'])
-    run(['fab', target, 'servers.stop_service:reup'])
+    run(["fab", target, "servers.stop_results"])
+    run(["fab", target, "servers.stop_service:reup"])
 
 
-@election.command('finish')
-@click.argument('date')
-@click.option('--test', is_flag=True, help='Pass test flag to elex')
+@election.command("finish")
+@click.argument("date")
+@click.option("--test", is_flag=True, help="Pass test flag to elex")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def election_finish(date, target, test):
     if test:
-        test_flag = '--test'
+        test_flag = "--test"
     else:
-        test_flag = ''
+        test_flag = ""
 
-    run(['fab', target, 'servers.stop_service:results'])
-    run(['fab', target, 'servers.stop_service:reup'])
+    run(["fab", target, "servers.stop_service:results"])
+    run(["fab", target, "servers.stop_service:reup"])
 
-    get_once = 'get_results {} {} --run_once'.format(date, test_flag)
-    reup_once = 'bootstrap_results_db {} {} --tabulated --run_once'.format(
+    get_once = "get_results {} {} --run_once".format(date, test_flag)
+    reup_once = "bootstrap_results_db {} {} --tabulated --run_once".format(
         date, test_flag
     )
-    run(['fab', target, 'django.management:{}'.format(get_once)])
-    run(['fab', target, 'django.management:{}'.format(reup_once)])
+    run(["fab", target, "django.management:{}".format(get_once)])
+    run(["fab", target, "django.management:{}".format(reup_once)])
 
 
-@election.command('zeroes')
-@click.argument('date')
-@click.option('--test', is_flag=True, help='Pass test flag to elex')
+@election.command("zeroes")
+@click.argument("date")
+@click.option("--test", is_flag=True, help="Pass test flag to elex")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def election_zeroes(date, target, test):
     if test:
-        test_flag = '--test'
+        test_flag = "--test"
     else:
-        test_flag = ''
+        test_flag = ""
 
-    get_zeroes = 'get_results {} {} --zeroes --run_once'.format(date, test_flag)
-    bake = 'bake_elections {}'.format(date)
-    run(['fab', target, 'django.management:{}'.format(get_zeroes)])
-    run(['fab', target, 'django.management:{}'.format(bake)])
+    get_zeroes = "get_results {} {} --zeroes --run_once".format(
+        date, test_flag
+    )
+    bake = "bake_elections {}".format(date)
+    run(["fab", target, "django.management:{}".format(get_zeroes)])
+    run(["fab", target, "django.management:{}".format(bake)])
 
 
-@election.command('replay')
-@click.argument('date')
-@click.option('--nobot', is_flag=True, help='Pass no-bot flag to reup')
+@election.command("replay")
+@click.argument("date")
+@click.option("--nobot", is_flag=True, help="Pass no-bot flag to reup")
 @click.option(
-    '--target', default='staging', help='The server environment to target'
+    "--target", default="staging", help="The server environment to target"
 )
 def election_test_replay(date, target, nobot):
     if nobot:
-        no_bot_flag = '--nobot'
+        no_bot_flag = "--nobot"
     else:
-        no_bot_flag = ''
+        no_bot_flag = ""
 
-    run(['fab', target, 'servers.start_results:{0},,--replay'.format(date)])
-    run(['fab', target, 'servers.start_reup:{0},{1}'.format(date, no_bot_flag)])
+    run(["fab", target, "servers.start_results:{0},,--replay".format(date)])
+    run(
+        ["fab", target, "servers.start_reup:{0},{1}".format(date, no_bot_flag)]
+    )
